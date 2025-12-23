7f62a8f58ecd970aaf24adb74f730c18d50fba89
AnthMendoza <AnthonyMend515@gmail.com>
2025-12-22 16:08:10 -0800

    Add lifetime implied PDF visualization for ticker

---
+++ examples/appl_PDF_set_exmaple.py
@@ -0,0 +1,26 @@
+from oipd import RND, MarketInputs, ModelParams , RNDResultSet
+import matplotlib.pyplot as plt
+from datetime import date
+
+
+ticker_ = "APPL"
+
+# 3 INPUTS:
+# 1. market parameters
+# 2. model parameters
+# 3. column mapping
+
+market = MarketInputs(
+    valuation_date=date.today(),
+    expiry_date=date(2026, 3, 20),
+    risk_free_rate=0.04199, # US 3-month nominal Treasury yield
+)
+
+# estimate RND Set
+est_appl = RNDResultSet.set_from_ticker(ticker_,market)
+
+
+
+est_appl.plot()
+
+plt.show()
